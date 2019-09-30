a9d27a64b2bb6e2ab6028a9f1d70bb23f1fb4158
Sergey Zenkovich <sapocalipsys.z@gmail.com>
2019-09-30 23:02:08 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let opens = [];
+  let closes = {};
+  let doubles = [];
+  for(var i = 0; i<bracketsConfig.length; i++){
+  	closes[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  	if (bracketsConfig[i][0] == bracketsConfig[i][1]){
+  		doubles.push(bracketsConfig[i][0]);
+  		opens.push(bracketsConfig[i][0])
+  	}
+  	else{
+  		opens.push(bracketsConfig[i][0]);
+  	}
+  }
+
+
+  let tempstr = '';
+  	for (var i = 0; i<str.length; i++){
+  			
+  		if (tempstr == "" && !opens.includes(str[i]) && !doubles.includes(str[i])){
+  			return false;
+  		}
+  		else if(tempstr != ""  && ( (str[i] == tempstr[tempstr.length-1] && doubles.includes(str[i]))|| !opens.includes(str[i]))) {
+  			let x = tempstr[tempstr.length-1];
+  			if (str[i] == closes[x]){
+  				tempstr = tempstr.slice(0,tempstr.length-1)
+  			}
+  		}
+  		else if(opens.includes(str[i]) ||  doubles.includes(str[i])){
+  			tempstr+=str[i];
+  		}
+  		
+  	}
+  return !(!!(tempstr));
 }
+
